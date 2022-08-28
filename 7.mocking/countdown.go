0d6ad1a605ab711a40ae3a2c5973bc6728b57e51
fyved24 <fyved24@163.com>
2022-08-28 15:28:54 +0800

    添加测试

--- 7.mocking/countdown.go
+++ 7.mocking/countdown.go
@@ -9,6 +9,9 @@ import (
 const finalWord = "Go!"
 const countdownStart = 3
 
+const write = "write"
+const sleep = "sleep"
+
 type Sleeper interface {
 	Sleep()
 }
@@ -19,6 +22,28 @@ type ConfigurableSleeper struct {
 func (o *ConfigurableSleeper) Sleep() {
 	time.Sleep(o.Duration)
 }
+
+type SpySleeper struct {
+	Calls int
+}
+
+func (s *SpySleeper) Sleep() {
+	s.Calls++
+}
+
+type CountdownOperationsSpy struct {
+	Calls []string
+}
+
+func (s *CountdownOperationsSpy) Sleep() {
+	s.Calls = append(s.Calls, sleep)
+}
+
+func (s *CountdownOperationsSpy) Write(p []byte) (n int, err error) {
+	s.Calls = append(s.Calls, write)
+	return
+}
+
 func Countdown(w io.Writer, sleeper Sleeper) {
 
 	for i := countdownStart; i > 0; i-- {
--- 7.mocking/countdown_test.go
+++ 7.mocking/countdown_test.go
@@ -5,30 +5,6 @@ import (
 	"testing"
 )
 
-type SpySleeper struct {
-	Calls int
-}
-
-func (s *SpySleeper) Sleep() {
-	s.Calls++
-}
-
-type CountdownOperationsSpy struct {
-	Calls []string
-}
-
-func (s *CountdownOperationsSpy) Sleep() {
-	s.Calls = append(s.Calls, sleep)
-}
-
-func (s *CountdownOperationsSpy) Write(p []byte) (n int, err error) {
-	s.Calls = append(s.Calls, write)
-	return
-}
-
-const write = "write"
-const sleep = "sleep"
-
 func TestCountdown(t *testing.T) {
 	spySleepPrinter := &CountdownOperationsSpy{}
 
